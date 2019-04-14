0a477c822a9e28027f4a5e8d37cb7d0cbe2aee01
Yair Taylor <yairtaylor@gmail.com>
2019-04-14 23:44:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,39 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |winplay|
+    board_pt = [board[winplay[0]],board[winplay[1]],board[winplay[2]]]
+    if board_pt.all?{|i| i=="X"} ||board_pt.all?{|i| i=="O"}
+      return winplay
+    end
+  end
+  return false
+end
+
+def full?(board)
+  if board.none?
+end
+
+def draw?
+  
+end
+
+def over?
+  
+end
+
+def winnner
+  
+end
