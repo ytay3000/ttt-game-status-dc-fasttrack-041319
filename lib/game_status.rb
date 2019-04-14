3415a80ad1899359d708df44e95ee165e3e6cd5b
Yair Taylor <yairtaylor@gmail.com>
2019-04-14 23:58:27 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,15 +36,17 @@ end
 def draw?(board)
   if won?(board)==false && full?(board)==true
     return true
-  elsif full?(board)==false
-    return false
-  else
+  else 
     return false
   end
 end
 
 def over?(board)
-  if won?(board).class==Array
+  if won?(board).class==Array || draw?(board)==true || full?(board)==true
+    return true
+  else
+    return false
+  end
 end
 
 def winnner
